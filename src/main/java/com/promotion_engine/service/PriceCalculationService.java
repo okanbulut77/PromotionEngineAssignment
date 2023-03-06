2a4dcc16096487089e59f2fce5009378a9f80473
okanbulut <okanbulut@gmail.com>
2023-03-06 10:21:57 +0000

    calculatePrice logic implemented

--- src/main/java/com/promotion_engine/service/PriceCalculationService.java
+++ src/main/java/com/promotion_engine/service/PriceCalculationService.java
@@ -1,21 +1,37 @@
 package com.promotion_engine.service;
 
 import com.promotion_engine.model.Cart;
+import com.promotion_engine.model.ProductItem;
+import com.promotion_engine.model.Promo;
+import com.promotion_engine.repository.IPromoRepository;
+
+import java.util.List;
 
 public class PriceCalculationService implements IPriceCalculationService {
+    IPromoRepository promoRepository;
+
+    public PriceCalculationService(IPromoRepository promoRepository) {
+        this.promoRepository = promoRepository;
+    }
+
     public double calculatePrice(Cart cart){
         // Calculate promoted price.
         double promotedPrice = 0; //TODO implement applyPromo(cart);
         double remainingProductsPrice = 0;
 
-        // TODO Calculate remaining unpromoted products price and assign to remainingProductPrice
+        // Calculate remaining unpromoted products price and assign to remainingProductPrice
+        for (ProductItem productItem: cart.getProductItems()) {
+            remainingProductsPrice += productItem.getProduct().getDiscountedPrice() * productItem.getAmount();
+        }
+
         return promotedPrice + remainingProductsPrice;
     }
 
     private double applyPromo(){
         double totalPromotedPrice = 0;
-
-        //TODO implement the logic
+        //Get promo list
+        List<Promo> promoList = this.promoRepository.getPromoList();
+        //TODO Get each promo and apply to the cart
 
         return totalPromotedPrice;
     }
