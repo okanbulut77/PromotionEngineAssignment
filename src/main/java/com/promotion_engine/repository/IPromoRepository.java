e7e4aa1c340d84bfeff9d6395c6178093d7c214a
okanbulut <okanbulut@gmail.com>
2023-03-06 09:44:33 +0000

    Promo Repository added

---
+++ src/main/java/com/promotion_engine/repository/IPromoRepository.java
@@ -0,0 +1,9 @@
+package com.promotion_engine.repository;
+
+import com.promotion_engine.model.Promo;
+
+import java.util.List;
+
+public interface IPromoRepository {
+    public List<Promo> getPromoList();
+}
---
+++ src/main/java/com/promotion_engine/repository/PromoRepository.java
@@ -0,0 +1,14 @@
+package com.promotion_engine.repository;
+
+import com.promotion_engine.model.Promo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PromoRepository implements IPromoRepository {
+    public List<Promo> getPromoList(){
+        List<Promo> promoList = new ArrayList<Promo>();
+        //TODO initialize Promotion List
+        return promoList;
+    }
+}
