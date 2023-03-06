f917333226257f7fa38e542633d9a8677b398224
okanbulut <okanbulut@gmail.com>
2023-03-06 11:40:44 +0000

    Added default promotion list

--- src/main/java/com/promotion_engine/repository/PromoRepository.java
+++ src/main/java/com/promotion_engine/repository/PromoRepository.java
@@ -1,14 +1,24 @@
 package com.promotion_engine.repository;
 
+import com.promotion_engine.model.Product;
+import com.promotion_engine.model.ProductItem;
 import com.promotion_engine.model.Promo;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class PromoRepository implements IPromoRepository {
     public List<Promo> getPromoList(){
         List<Promo> promoList = new ArrayList<Promo>();
-        //TODO initialize Promotion List
+        // initialize Promotion List
+
+        promoList.add(new Promo(Arrays.asList(new ProductItem(new Product("A", 50, 0), 3)), 130, false));
+        promoList.add(new Promo(Arrays.asList(new ProductItem(new Product("B", 50, 0), 2)), 45, false));
+        promoList.add(new Promo(Arrays.asList(
+                new ProductItem(new Product("C", 20, 0), 1),
+                new ProductItem(new Product("D", 15, 0), 1)), 30, true));
+
         return promoList;
     }
 }
