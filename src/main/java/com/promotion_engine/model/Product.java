e234fd41c52dbaff81b1015d3962dbdd6549cf35
okanbulut <okanbulut@gmail.com>
2023-03-06 09:27:19 +0000

    Constructors, Setters and Getters added

--- src/main/java/com/promotion_engine/model/Product.java
+++ src/main/java/com/promotion_engine/model/Product.java
@@ -4,4 +4,34 @@ public class Product {
     private String sku;
     private double unitPrice;
     private double discount;
+
+    public Product(String sku, double unitPrice, double discount) {
+        this.sku = sku;
+        this.unitPrice = unitPrice;
+        this.discount = discount;
+    }
+
+    public String getSku() {
+        return sku;
+    }
+
+    public void setSku(String sku) {
+        this.sku = sku;
+    }
+
+    public double getUnitPrice() {
+        return unitPrice;
+    }
+
+    public void setUnitPrice(double unitPrice) {
+        this.unitPrice = unitPrice;
+    }
+
+    public double getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(double discount) {
+        this.discount = discount;
+    }
 }
--- src/main/java/com/promotion_engine/model/ProductItem.java
+++ src/main/java/com/promotion_engine/model/ProductItem.java
@@ -3,4 +3,25 @@ package com.promotion_engine.model;
 public class ProductItem {
     private  Product product;
     private int amount;
+
+    public ProductItem(Product product, int amount) {
+        this.product = product;
+        this.amount = amount;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
 }
