399a7f825e6c25a71bba921ea82e424a3368ea21
okanbulut <okanbulut@gmail.com>
2023-03-06 08:55:28 +0000

    Initial Commit

---
+++ src/main/java/com/promotion_engine/model/Product.java
@@ -0,0 +1,7 @@
+package com.promotion_engine.model;
+
+public class Product {
+    private String sku;
+    private double unitPrice;
+    private double discount;
+}
---
+++ src/main/java/com/promotion_engine/model/ProductItem.java
@@ -0,0 +1,6 @@
+package com.promotion_engine.model;
+
+public class ProductItem {
+    private  Product product;
+    private int amount;
+}
