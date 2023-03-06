b29b5196ff8839337b180d88f9916275636fc226
okanbulut <okanbulut@gmail.com>
2023-03-06 11:18:57 +0000

    Promotion classes addes

--- src/main/java/com/promotion_engine/service/BundledPromoService.java
+++ src/main/java/com/promotion_engine/service/BundledPromoService.java
@@ -1,10 +1,35 @@
 package com.promotion_engine.service;
 
 import com.promotion_engine.model.Cart;
+import com.promotion_engine.model.ProductItem;
 import com.promotion_engine.model.Promo;
 
 public class BundledPromoService implements IPromoService{
     public double calculatePromotedPrice(Cart cart, Promo promo){
-        return 0.00;
+        int numberOfPromos = 0;
+        // Multiple products promotion type
+        for(ProductItem productItem : promo.getPromotionProductItemList()) {
+            // get the required amount of each product to meet promotion requirements
+            int requiredAmount = productItem.getAmount();
+            ProductItem searchItem = cart.findProductItem(productItem);
+
+            if(searchItem == null) {
+                return 0;
+            }
+            int cartAmount = searchItem.getAmount();
+            //Depending on the products amount in the Cart there can be 1 or more bundle combinations.
+            // E.g. 3 C and 2 D means (C+D)X2 promo and 1 remaining C
+            if(cartAmount / requiredAmount < numberOfPromos || numberOfPromos == 0) {
+                numberOfPromos = cartAmount / requiredAmount;
+            }
+        }
+        // remove the items which are part of multi product promotion combination (Apply only one promotion at a time!!!)
+        for (ProductItem productItem : promo.getPromotionProductItemList()) {
+            ProductItem cartProductItemItem = cart.findProductItem(productItem);
+            if(cartProductItemItem != null) {
+                cartProductItemItem.setAmount(cartProductItemItem.getAmount() - numberOfPromos * productItem.getAmount());
+            }
+        }
+        return numberOfPromos * promo.getPromoPrice();
     }
 }
--- src/main/java/com/promotion_engine/service/SingleProductPromoService.java
+++ src/main/java/com/promotion_engine/service/SingleProductPromoService.java
@@ -1,10 +1,26 @@
 package com.promotion_engine.service;
 
 import com.promotion_engine.model.Cart;
+import com.promotion_engine.model.ProductItem;
 import com.promotion_engine.model.Promo;
 
 public class SingleProductPromoService implements IPromoService{
     public double calculatePromotedPrice(Cart cart, Promo promo){
-        return 0.00;
+
+        int numberOfPromos = 0;
+        //
+        ProductItem promotionProductItem = promo.getPromotionProductItemList().get(0);
+        int promotionQuantity = promotionProductItem.getAmount();
+        ProductItem searchItem = cart.findProductItem(promotionProductItem);
+
+        if(searchItem != null) {
+            int cartQuantity = searchItem.getAmount();
+            numberOfPromos = cartQuantity / promotionQuantity;
+            int remainingItems = cartQuantity % promotionQuantity;
+            // reset the remaining items to cart again to calculate their price without offer
+            searchItem.setAmount(remainingItems);
+        }
+
+        return numberOfPromos * promo.getPromoPrice();
     }
 }
